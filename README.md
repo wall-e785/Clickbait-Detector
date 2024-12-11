2edf8a316a63b855ebf1a67bf52480b2174ba245
wall-e785 <wall_echau604@yahoo.ca>
2024-12-10 23:59:14 -0800

    Updated file structure for main

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Clickbait-Detector
 IAT 360 Final Project Fall 2024
+Clickbait Detector! Enter an article title, and the BERT model, trained in sentiment analysis will classify the article into NOT or CLICKBAIT classes.
+
+
+Created by Jasper Gass & Wallace Chau
--- clickbaitdetector.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-# let's load the imdb dataset from huggingface
-# source: (https://huggingface.co/datasets/imdb)
-raw_dataset = load_dataset('imdb')
-raw_dataset
-from datasets import load_dataset
-ds = load_dataset("christinacdl/clickbait_detection_dataset")
